534f7c1f96086ddec6625fb05da6d0d5ed6bbb84
avikaufman <avi-kaufman@uiowa.edu>
2024-09-02 17:57:59 -0500

    Method Comments
    Added comments for each method

--- Proxy.py
+++ Proxy.py
@@ -9,29 +9,38 @@ class Proxy:
         self.shannon_all = None
 
     def get_numatoms(self):
+        # Method to get the number of atoms
         return
 
     def get_numparts(self):
+        # Method to get the number of parts
         return
 
     def ordered_bitstring(self):
+        # Method to get the ordered bitstring
         return
 
     def combos(self):
+        # Method to get the combinations
         return
 
     def shannon_all(self):
+        # Method to calculate Shannon entropy for the entire system
         return
 
     def shannon_in(self):
+        # Method to calculate Shannon entropy for the in subsytem
         return
 
     def shannon_out(self):
+        # Method to calculate Shannon entropy for out subsytem
         return
 
     def calculate_proxy(self):
+        # Method to calculate the proxy
         return
 
     def set_part(self, part):
+        # Method to set the part attribute
         self.part = part
         return
--- VonNeumann.py
+++ VonNeumann.py
@@ -8,19 +8,25 @@ class VonNeumann:
         self.groundstate = None
 
     def get_hamiltonian(self):
+        # Method to get the Hamiltonian
         return
 
     def get_groundstate(self):
+        # Method to get the ground state from the Hamiltonian
         return
 
     def get_CMatrix(self):
+        # Method to get the C Matrix from the ground state
         return
 
     def get_reduced_density(self):
+        # Method to get the reduced density matrix from the C Matrix
         return
 
     def get_eigenvalues(self):
+        # Method to get the eigenvalues from the reduced density matrix
         return
 
     def get_von_neumann(self):
+        # Method to calculate the Von Neumann entropy from the eigenvalues
         return
